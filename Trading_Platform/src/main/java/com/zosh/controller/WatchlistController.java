eacf20f7f7fc8e3ce1b48770dc2802c48387e374
KAIBALYA <KAIBALYA@LAPTOP-M7B1I3IS>
2025-07-11 11:58:18 +0530

    commit

---
+++ Trading_Platform/src/main/java/com/zosh/controller/WatchlistController.java
@@ -0,0 +1,60 @@
+package com.zosh.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zosh.modal.Coin;
+import com.zosh.modal.User;
+import com.zosh.modal.Watchlist;
+import com.zosh.service.CoinService;
+import com.zosh.service.UserService;
+import com.zosh.service.WatchlistService;
+
+@RestController
+public class WatchlistController {
+	
+	@Autowired
+	private WatchlistService watchlistService;
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private CoinService coinService;
+	
+	@GetMapping("/user")
+	public ResponseEntity<Watchlist> getUserWatchlist(@RequestHeader("Authorization") String jwt) throws Exception{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		Watchlist watchlist=watchlistService.findUserWatchlist(user.getId());
+		return ResponseEntity.ok(watchlist);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<Watchlist> createWatchlist(@RequestHeader("Authorization")String jwt) throws Exception{
+		User user=userService.findUserProfileByJwt(jwt);
+		Watchlist createdWatchlist=watchlistService.createWatchlist(user);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdWatchlist);
+	}
+	
+	@GetMapping("/{watchlistId}")
+	public ResponseEntity<Watchlist> getWatchlistById(@PathVariable Long watchlistId) throws Exception{
+		Watchlist watchlist=watchlistService.findById(watchlistId);
+		return ResponseEntity.ok(watchlist);
+	}
+	
+	@PatchMapping("/add/coin/{coinId}")
+	public ResponseEntity<Coin> addItemToWatchlist(@RequestHeader("Authorization")String jwt,@PathVariable String coinId) throws Exception{
+		User user=userService.findUserProfileByJwt(jwt);
+		Coin coin=coinService.findById(coinId);
+		Coin addedCoin=watchlistService.addCoinToWatchlist(coin, user);
+		return ResponseEntity.ok(addedCoin);
+	}
+}
---
+++ Trading_Platform/src/main/java/com/zosh/service/WatchlistService.java
@@ -0,0 +1,16 @@
+package com.zosh.service;
+
+import com.zosh.modal.Coin;
+import com.zosh.modal.User;
+import com.zosh.modal.Watchlist;
+
+public interface WatchlistService {
+	
+	Watchlist findUserWatchlist(Long userId) throws Exception;
+	
+	Watchlist createWatchlist(User user);
+	
+	Watchlist findById(Long id) throws Exception;
+	
+	Coin addCoinToWatchlist(Coin coin,User user) throws Exception;
+}
